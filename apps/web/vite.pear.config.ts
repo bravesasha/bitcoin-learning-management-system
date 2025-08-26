9f381bdc2b1c5e443bf62f56695d885205f7f0bd
trigger67 <10185784+trigger67@users.noreply.github.com>
2025-08-26 18:33:05 +0200

    wip

---
+++ apps/web/vite.pear.config.ts
@@ -0,0 +1,60 @@
+/// <reference types="vite-plugin-svgr/client" />
+
+import { tanstackRouter } from '@tanstack/router-plugin/vite';
+import react from '@vitejs/plugin-react';
+// import { visualizer } from 'rollup-plugin-visualizer';
+import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+const UI_PACKAGE_ASSETS = '../../packages/ui/src/assets';
+
+export default defineConfig({
+  assetsInclude: [UI_PACKAGE_ASSETS],
+  base: '/',
+  build: {
+    chunkSizeWarningLimit: 600,
+    cssCodeSplit: true,
+    outDir: 'dist',
+    reportCompressedSize: true,
+    target: 'esnext',
+    rollupOptions: {
+      output: {
+        inlineDynamicImports: true,
+      },
+    },
+  },
+  plugins: [
+    svgr(),
+    tsconfigPaths({
+      root: './',
+    }),
+    tanstackRouter({
+      // If set to true, creates several issues with the router / i18n
+      autoCodeSplitting: false,
+      target: 'react',
+    }),
+    react(),
+
+    // Useful for analyzing bundle size
+    // visualizer({
+    //   filename: 'dist/stats.html',
+    //   open: true,
+    //   gzipSize: true,
+    // }),
+  ],
+
+  root: process.cwd(),
+
+  server: {
+    host: '0.0.0.0',
+    port: 8181,
+    proxy: {
+      '/api': `http://${process.env.DOCKER ? 'api' : 'localhost'}:3000`,
+      '/cdn': {
+        rewrite: (path) => path.replace(/^\/cdn/, ''),
+        target: `http://${process.env.DOCKER ? 'cdn:80' : 'localhost:8080'}`,
+      },
+    },
+  },
+});
